cbddbcc54e329901c850b49af1bafa61e8e8248c
jipark0716 <jipark0716@gmail.com>
2025-01-13 20:07:24 +0900

    feat: init commit

---
+++ src/main/kotlin/kr/co/rainbowletter/api/RainbowletterApiApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.rainbowletter.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RainbowletterApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RainbowletterApiApplication>(*args)
+}
