8ebe6e520d1fcb024014bd1d68cc691aa2ad4985
jipark0716 <jipark0716@gmail.com>
2025-01-16 20:22:28 +0900

    feat: 환경변수 예시 추가

--- src/main/kotlin/kr/co/rainbowletter/api/auth/User.kt
+++ src/main/kotlin/kr/co/rainbowletter/api/auth/User.kt
@@ -4,8 +4,8 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class User(
-    private val email: String,
-    private val roles: String,
+    public val email: String,
+    public val roles: String,
 ) : UserDetails {
     override fun getAuthorities(): MutableCollection<GrantedAuthority> = ArrayList()
     override fun getPassword(): String = ""
