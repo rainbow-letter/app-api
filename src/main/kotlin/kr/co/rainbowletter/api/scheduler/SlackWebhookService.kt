eb138167be69f1ab69dab3cb35abf01116cf03c4
유현 <yh7119@gmail.com>
2025-04-06 16:39:59 +0900

    rename: 슬랙 알림 채널 분리에 의한 Client 및 URL 변경

--- src/main/kotlin/kr/co/rainbowletter/api/scheduler/SlackWebhookService.kt
+++ src/main/kotlin/kr/co/rainbowletter/api/scheduler/SlackWebhookService.kt
@@ -1,6 +1,6 @@
 package kr.co.rainbowletter.api.scheduler
 
-import kr.co.rainbowletter.api.client.SlackFeignClient
+import kr.co.rainbowletter.api.client.SlackReviewClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 @Service
 class SlackWebhookService(
     private val slackMessageFormatter: SlackMessageFormatter,
-    private val slackFeignClient: SlackFeignClient
+    private val slackReviewClient: SlackReviewClient
 ) {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
@@ -17,7 +17,7 @@ class SlackWebhookService(
         val payload = mapOf("text" to message)
 
         try {
-            slackFeignClient.sendSlackMessage(payload)
+            slackReviewClient.sendSlackMessage(payload)
             logger.info("Slack 메시지 전송 성공")
         } catch (e: Exception) {
             logger.error("Slack 메시지 전송 실패: ${e.message}")
